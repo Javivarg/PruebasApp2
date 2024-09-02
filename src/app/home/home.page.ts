f37dc6569248f98ac7775fd775fecbf9b401b146
Javivarg <javi.vargasv@duocuc.cl>
2024-09-01 22:25:45 -0400

    n°1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+// Define la interfaz UserForm aquí o impórtala desde otro archivo
+interface UserForm {
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +13,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  // Define el modelo de los datos del formulario
+  userForm: UserForm = {
+    email: '',
+    password: ''
+  };
+
   constructor() {}
 
+  // Método para manejar el envío del formulario
+  onSubmit() {
+    console.log('Email:', this.userForm.email);
+    console.log('Password:', this.userForm.password);
+  }
 }
+
